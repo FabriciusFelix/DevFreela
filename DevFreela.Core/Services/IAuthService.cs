ebd91823190b742506eb7242a642e4b205046d7c
Fabricius Felix <Fabricius.felix@gmail.com>
2023-08-04 18:22:52 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Core/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email, string role);
+        string ComputeSha256Hash(string password);
+    }
+}
